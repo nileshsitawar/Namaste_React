01b9df4bd96e38412b521c60a21d826578835c2e
nileshsitawar <65218382+nileshsitawar@users.noreply.github.com>
2023-11-04 15:49:09 +0530

    Update index.js

--- Episode-01/index.js
+++ Episode-01/index.js
@@ -2,4 +2,5 @@
  const head1 = React.createElement("h1",{},"hello world from React");
  // work of reactdom to create root for react lib to put this h1 into browser
  const root = ReactDOM.createRoot(document.getElementById("root"));
+//  rendering the react ELEment in root
  root.render(head1)
