60b9fd370462fcf225ec3fe40f8d8731794f2afa
nileshsitawar <65218382+nileshsitawar@users.noreply.github.com>
2023-11-04 15:47:29 +0530

    Update App.js

--- Episode-01/App.js
+++ Episode-01/App.js
@@ -1,8 +1,23 @@
  // core react thing
- const head1 = React.createElement("h1",{id:"hell",xyz:
-"ss"},"Hakuna matata from React");
- // work of reactdom to create root for react lib to put this h1 into browser
- const root = ReactDOM.createRoot(document.getElementById("root"));
- root.render(head1)
- 
-console.log(head1)
+//  const head1 = React.createElement("h1",{id:"hell",xyz:
+// "ss"},"Hakuna matata from React");
+//  // work of reactdom to create root for react lib to put this h1 into browser
+//  const root = ReactDOM.createRoot(document.getElementById("root"));
+//  root.render(head1)
+//  g
+// console.log(head1)
+
+const parent = React.createElement("div",{id:"parent"},[
+React.createElement("div",{id:"child1"}, [
+    React.createElement("h1",{},"I am h1 tag"),
+    React.createElement("h2",{},"i am h2 tag"),
+]),
+React.createElement("div",{id:"child2"}, [
+    React.createElement("h1",{},"I am h1 tag"),
+    React.createElement("h2",{},"i am h2 tag")
+]),
+]);
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(parent)
